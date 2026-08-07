5ae69948d3c13d6f7ade51a2d9539e0c91fd38db
Deepika S <dd770514@gmail.com>
2026-08-07 20:18:21 +0530

    Deleted

--- new.py
+++
@@ -1,9 +0,0 @@
-file=open("hello.txt","w")
-file.write("Hello Deepika")
-file.close()
-file=open("hello.txt","a")
-file.write("\nWelcome to Python \n LookUp")
-file.close()
-with open("hello.txt","r") as f:
-    print(f.read())
-                    
